4b2b657cf3cf19e9d2bdf8186ee2953afd1a4750
Nogueira, Admilson M Jr <nogueirajr@hotmail.com>
2024-12-04 10:53:20 -0300

    Implementation according to specification.

--- Questao1/ContaBancaria.cs
+++ Questao1/ContaBancaria.cs
@@ -2,8 +2,43 @@
 
 namespace Questao1
 {
-    class ContaBancaria {
+    class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
 
-       
+        private const double TaxaSaque = 3.50;
+
+        // Construtor para conta com depósito inicial
+        public ContaBancaria(int numero, string titular, double depositoInicial)
+        {
+            Numero = numero;
+            Titular = titular;
+            Deposito(depositoInicial);
+        }
+
+        // Construtor para conta sem depósito inicial
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0.0;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + TaxaSaque;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {Numero}, Titular: {Titular}, Saldo: $ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
     }
 }
